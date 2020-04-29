49b3df802b2f641f40be9590bf6b50124c5cbdb4
shubhamkakkar <shubham.kakkar98@gmail.com>
2020-04-29 16:16:54 +0530

    precall code cleanup

--- android/app/src/main/java/com/rnnotifications/MainActivity.java
+++ android/app/src/main/java/com/rnnotifications/MainActivity.java
@@ -2,11 +2,6 @@ package com.rnnotifications;
 
 import com.facebook.react.ReactActivity;
 
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
-import android.os.Build;
-import android.os.Bundle;
-
 public class MainActivity extends ReactActivity {
 
   /**
@@ -16,18 +11,5 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "RNNotifications";
-  }
-
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {    
-      NotificationChannel notificationChannel = new NotificationChannel("500", "MainChannel", NotificationManager.IMPORTANCE_HIGH);
-      notificationChannel.setShowBadge(true);
-      notificationChannel.enableVibration(true);
-      notificationChannel.enableLights(true);
-      notificationChannel.setVibrationPattern(new long[]{400, 200, 400});
-      NotificationManager manager = getSystemService(NotificationManager.class);
-      manager.createNotificationChannel(notificationChannel);
-    }      
   }
 }
